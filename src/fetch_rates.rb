6ade6083fe515d6bc05a75e7bd6e18191621bf48
frogue <zzmaric@gmail.com>
2013-10-01 01:48:17 +0200

    Implemented loading exchange rates api app_id and Heroku DB password from YAML.

--- src/fetch_rates.rb
+++ src/fetch_rates.rb
@@ -1,20 +1,21 @@
 load 'modules/fetching.rb'
 load 'modules/storing.rb'
-require 'io/console'
 require 'date'
+require 'yaml'
 
-currs = ['HRK', 'EUR', 'USD', 'CHF', 'GBP', 'AUD', 'CAD', 'BRL']
+CURRS = ['HRK', 'EUR', 'USD', 'CHF', 'GBP', 'AUD', 'CAD', 'BRL']
+PROPS = 'keys.yml'
 
-puts 'Open Exchange Rates App ID: '
-app_id = STDIN.noecho(&:gets).chomp
-puts 'Postgres DB password: '
-pass = STDIN.noecho(&:gets).chomp
+props = YAML.load_file(PROPS)
+
+app_id = props['app_id']
+pass = props['db_pass']
 
 date = '2013-09-28'
 res = Fetching.fetch_historical(app_id, date)
 base = res['base']
-# Extract only the currencies found in currs.
-selected = res['rates'].select { |code, rate| currs.include? code }
+# Extract only the currencies found in CURRS.
+selected = res['rates'].select { |code, rate| CURRS.include? code }
 conn = Storing.connect(pass)
 Storing.store_means(conn, base, selected, date)
 
