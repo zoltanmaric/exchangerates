f43e80d31f587a93b02fbbea2209ff69bee6dde7
frogue <zzmaric@gmail.com>
2013-10-03 02:47:32 +0200

    Created queries for fetching interesting data for various periods.

---
+++ queries/minmax 1 month.sql
@@ -0,0 +1,6 @@
+﻿select cur_code, min(rat_buy) as min_buy, min(rat_sell) as min_sell,
+	max(rat_buy) as max_buy, max(rat_sell) as max_sell
+	from rates join currencies on rat_target_cur_id = cur_id
+	where rat_date >= now()::date - interval '1 month'
+	group by cur_code
+	order by cur_code
